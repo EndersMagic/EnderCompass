a115be962e71ee9d5cbd33b2821a5d55b2dcceda
hohserg <hohserg1@yandex.ru>
2020-06-02 09:29:28 +0300

    Remove unused

--- src/main/java/hohserg/endercompass/Main.java
+++ src/main/java/hohserg/endercompass/Main.java
@@ -11,7 +11,6 @@ import static hohserg.endercompass.Main.ID;
 @Mod(ID)
 public class Main {
     public static final String ID = "endercompass";
-    public static final String VERSION = "1.0";
     public static final String NAME = "Ender Compass";
 
     public static final Random random = new Random();
